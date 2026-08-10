91376e4df859302e64e34c674711385f2818aba6
hathienloc131 <ha.thienloc131@gmail.com>
2026-08-10 11:11:54 +0700

    [Collect] third view

---
+++ script_collect_3_tasks_gpu0.sh
@@ -0,0 +1,3 @@
+bash collect_data.sh adjust_bottle demo_clean 0
+bash collect_data.sh grab_roller demo_clean 0
+bash collect_data.sh move_stapler_pad demo_clean 0
---
+++ script_collect_3_tasks_gpu1.sh
@@ -0,0 +1,3 @@
+bash collect_data.sh open_laptop demo_clean 1
+bash collect_data.sh place_mouse_pad demo_clean 1
+bash collect_data.sh place_object_basket demo_clean 1
